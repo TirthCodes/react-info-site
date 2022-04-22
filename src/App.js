1908387f302c8d30b8be258846d7c86746e2be63
Tirth Patel <tirthpatelcse@gmail.com>
2022-04-22 14:37:55 +0530

    Organize Components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-
-function Header() {
-  return (
-    <header>
-      <nav>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <ul className='nav-items'>
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2022 tirth's development. All rights reserved.</small>
-    </footer>
-    )
-}
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <footer>
+        <small>© 2022 tirth's development. All rights reserved.</small>
+      </footer>
+      )
+}
+
+export default Footer;
